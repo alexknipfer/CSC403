acbe21f898491a2cd7363c520859f19e2885bc9e
Alex Knipfer <alexanderknipfer@gmail.com>
2016-11-29 21:26:23 -0600

    create matrix adjacency of all nodes of network

--- Network-Connections/MyNetwork.hpp
+++ Network-Connections/MyNetwork.hpp
@@ -8,12 +8,20 @@ using namespace std;
 class MyNetwork{
     public:
         void loadData(int, int);
-        void calcDistance(int, int, int, int);
+        double calcDistance(int, int, int, int);
+        void calcShortest();
         void printVector();
 
     private:
         vector<int> xCoordinates;
         vector<int> yCoordinates;
+        struct data{
+            int x1, x2;
+            int y1, y2;
+            double distance;
+        };
+        data myGraph[150][150];
+        vector< vector<data> > graph;
 };
 
 #endif
--- Network-Connections/network.cpp
+++ Network-Connections/network.cpp
@@ -29,6 +29,7 @@ int main(){
         }
 
         network.printVector();
+        network.calcShortest();
 
             //get next network
         inputFile >> numOfComputers;
@@ -51,7 +52,36 @@ void MyNetwork::loadData(int x, int y){
 
 //******************************************************************************
 
-void MyNetwork::calcDistance(int x1, int x2, int y1, int y2){
+void MyNetwork::calcShortest(){
+    int iterate = 0;
+    int maxSize = xCoordinates.size();
+
+    //double smallest = calcDistance(xCoordinates[0], xCoordinates[1], yCoordinates[0], yCoordinates[1]);
+    for(int y = 0; y < xCoordinates.size(); y++){
+        for(int x = iterate; x < xCoordinates.size() - 1; x++){
+            double currDistance = calcDistance(xCoordinates[iterate], xCoordinates[x + 1], yCoordinates[iterate], yCoordinates[x + 1]);
+            data myData;
+            myData.x1 = xCoordinates[iterate];
+            myData.x2 = xCoordinates[x + 1];
+            myData.y1 = yCoordinates[iterate];
+            myData.y2 = yCoordinates[x + 1]; 
+            myData.distance = currDistance;
+            myGraph[iterate][x] = myData;
+        }
+        iterate++;
+    }
+
+    for(int x = 0; x < xCoordinates.size(); x++){
+        for(int y = 0; y < yCoordinates.size(); y++){
+            cout << myGraph[x][y].distance << " ";
+        }
+        cout << endl;
+    }
+}
+
+//******************************************************************************
+
+double MyNetwork::calcDistance(int x1, int x2, int y1, int y2){
         //Receives - x and y coordinates
         //Task - calculate the distance between two points
         //Returns - nothing
@@ -60,6 +90,8 @@ void MyNetwork::calcDistance(int x1, int x2, int y1, int y2){
     int xDistance = (x2 - x1) * (x2 - x1);
     int yDistance = (y2 - y1) * (y2 - y1);
     distance = sqrt(xDistance + yDistance);
+
+    return distance;
 }
 
 //******************************************************************************
